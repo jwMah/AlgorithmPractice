30090763df1fd27e791ee628c0c925eec10c40b1
jwMah <wigbuldok@naver.com>
2021-09-01 15:12:20 +0900

    bj1463 DP - bottom up 방식

--- src/bj1463/Main.java
+++ src/bj1463/Main.java
@@ -1,7 +1,24 @@
 package bj1463;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N+1];
+        arr[1] = 0;
+        for(int i=2;i<=N;i++){
+            arr[i] = arr[i-1] + 1;
+            if(i % 2 == 0) {
+                arr[i] = Math.min(arr[i],arr[i/2] + 1);
+            }
+            if(i % 3 == 0) {
+                arr[i] = Math.min(arr[i],arr[i/3] + 1);
+            }
+        }
+
+        System.out.println(arr[N]);
     }
 }
