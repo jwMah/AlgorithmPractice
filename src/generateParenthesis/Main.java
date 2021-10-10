5c112381771ed028b367b0438de2bd2125f018a2
jwMah <wigbuldok@naver.com>
2021-10-10 17:35:23 +0900

    주어진 개수의 맞는 가능한 괄호 경우의 수

--- src/generateParenthesis/Main.java
+++ src/generateParenthesis/Main.java
@@ -1,12 +1,33 @@
 package generateParenthesis;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static List<String> list;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
+        //int n = sc.nextInt();
+        Main a = new Main();
+        a.generate(4);
+        System.out.println(list);
         
+    }
+    public void generate(int n) {
+        list = new ArrayList<>();
+        dfs("",n,n);
+
+    }
+    public void dfs(String result,int left, int right) {
+        if(left < 0 || right < 0 || left > right) {
+            return;
+        }
+        if(left == 0 && right == 0) {
+            list.add(result);
+            return;
+        }
+        dfs(result+"(",left - 1,right);
+        dfs(result+")",left,right-1);
     }
 }
