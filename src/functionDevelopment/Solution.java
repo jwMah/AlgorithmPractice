654adde95b2c710e9fa9f11248394a709ee0b977
jwMah <wigbuldok@naver.com>
2022-01-05 17:06:02 +0900

    프로그래머스/스택큐/기능개발

---
+++ src/functionDevelopment/Solution.java
@@ -0,0 +1,26 @@
+package functionDevelopment;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
+            }
+            dayOfend[day]++;
+        }
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+    }
+    public static void main(String[] args) {
+        Solution a = new Solution();
+        int[] ans = a.solution(new int[]{93, 30, 55}, new int[]{1,30,5});
+        for (int an : ans) {
+            System.out.println(an);
+        }
+        
+
+    }
+}
